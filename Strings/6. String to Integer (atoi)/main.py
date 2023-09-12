969411e82d8e9f912f15867513f463341c027399
ekaterina <katyakrilova7@gmail,com>
2023-09-12 21:52:21 +0300

    Array

---
+++ Strings/6. String to Integer (atoi)/main.py
@@ -0,0 +1,11 @@
+# Найти индекс первого вхождения подстроки needle в haystack
+
+def strStr(haystack, needle):
+    index = haystack.find(needle)
+    return index
+
+
+
+
+print(strStr("sadbutsad", "sad"))
+print(strStr("leetcode", "leeto"))
---
+++ Strings/7. Implement strStr()/main.py
@@ -0,0 +1,15 @@
+# Найти наибольший общий префикс у строк (одинаковое начало)
+
+def longestCommonPrefix(strs):
+    prefix = ""
+    strs = [[str(s)] for s in strs]
+
+
+
+
+
+
+
+
+print(longestCommonPrefix(["flower", "flow", "flight"]))
+print(longestCommonPrefix(["dog", "racecar", "car"]))
